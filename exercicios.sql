255f9e03c075a1fba5d03d4147a9acc3eff707fb
Erickify <erick.silva@sptech.school>
2025-03-17 15:18:07 -0300

    alteração exercicio 01 banco de dados JOIN

--- exercicios.sql
+++ exercicios.sql
@@ -1,26 +1,21 @@
-Exercício 1.
 
-
--- Escreva e execute os comandos para:
--- • Criar a chave estrangeira na tabela correspondente conforme modelagem;
--- • Atualizar o país de todos os atletas;
--- • Exibir os atletas e seu respectivo país;
--- • Exibir apenas o nome do atleta e o nome do respectivo país;
--- • Exibir os dados dos atletas, seus respectivos países, de uma determinada capital;
-
-
-
-
-
+create database sprint2;
 
 USE sprint2;
 
--- • Criar a tabela chamada Atleta para conter os dados: idAtleta (int e chave
--- primária da tabela), nome (varchar, tamanho 40), modalidade (varchar, tamanho
--- 40), qtdMedalha (int, representando a quantidade de medalhas que o atleta
--- possui)
+CREATE TABLE tbPais(
+    idPais INT PRIMARY KEY AUTO_INCREMENT,
+    nomePais VARCHAR(30) NOT NULL,
+    capitalPais VARCHAR(40) NOT NULL
+);
 
--- 1:N
+INSERT INTO tbPais (nomePais, capitalPais) VALUES
+    ('Argentina', 'Buenos Aires'),
+    ('Brasil', 'Brasília'),
+    ('França', 'Paris'),
+    ('Alemanha', 'Berlim'),
+    ('Japão', 'Tóquio');
+
 CREATE TABLE tbAtleta(
     idAtleta INT PRIMARY KEY AUTO_INCREMENT,
     nomeAtleta VARCHAR(40) NOT NULL,
@@ -29,8 +24,8 @@ CREATE TABLE tbAtleta(
     fkPais INT
 );
 
--- • Inserir dados na tabela, procurando colocar mais de um atleta para cada
--- modalidade e pelo menos 5 atletas.
+ALTER TABLE tbAtleta ADD foreign key (fkPais)
+		references tbPais(idPais);
 
 INSERT INTO tbAtleta (nomeAtleta, modalidadeAtleta, qntMedalha, fkPais) VALUES
     ('Armando','Futebol', 5, 1),
@@ -40,30 +35,19 @@ INSERT INTO tbAtleta (nomeAtleta, modalidadeAtleta, qntMedalha, fkPais) VALUES
     ('Antoni','Futebol', 7, 4);
 
 
--- • Criar uma tabela chamada País para conter os dados: idPais (int e chave primária
--- da tabela), nome (varchar, tamanho 30), capital (varchar, tamanho 40);
+select * from tbAtleta;
 
-CREATE TABLE tbPais(
-    idPais INT PRIMARY KEY AUTO_INCREMENT,
-    nomePais VARCHAR(30) NOT NULL,
-    capitalPais VARCHAR(40) NOT NULL
-);
+select * from tbPais;
 
--- • Inserir pelo menos 4 países na tabela país.
-INSERT INTO tbPais (nomePais, capitalPais) VALUES
-    ('Argentina', 'Buenos Aires'),
-    ('Brasil', 'Brasília'),
-    ('França', 'Paris'),
-    ('Alemanha', 'Berlim'),
-    ('Japão', 'Tóquio');
+select * from tbAtleta as a
+join tbPais as p
+on a.fkPais = p.idPais;
+ 
+select a.nomeAtleta as Atleta, p.nomePais as Pais from tbAtleta as a
+join tbPais as p
+on a.fkPais = p.idPais; 
 
-
-
--- Fazer a modelagem lógica conforme a regra de negócio:
--- • 1 país tem 1 ou muitos atletas;
--- • 1 atleta é de 1 e somente 1 país;
-ALTER TABLE tbAtleta ADD foreign key (fkPais)
-		references tbPais(idPais);
-
-
+select a.nomeAtleta as Atleta, p.nomePais as Pais, p.capitalPais as Capital from tbAtleta as a
+join tbPais as p
+on a.fkPais = p.idPais; 
 
