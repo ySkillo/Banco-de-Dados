d095e24e1a9a7cb59f450ca988a71e649c4d5181
Erickify <erick.silva@sptech.school>
2025-03-17 16:09:02 -0300

    2 exercicio de banco de dados

--- exercicios.sql
+++ exercicios.sql
@@ -49,5 +49,84 @@ on a.fkPais = p.idPais;
 
 select a.nomeAtleta as Atleta, p.nomePais as Pais, p.capitalPais as Capital from tbAtleta as a
 join tbPais as p
-on a.fkPais = p.idPais; 
+on a.fkPais = p.idPais;
+
+/*
+Exercício 2:
+No MySQL Workbench, utilizando o banco de dados ‘sprint2’:
+• Criar a tabela chamada Musica para conter os dados: idMusica, titulo (tamanho
+40), artista (tamanho 40), genero (tamanho 40), sendo que idMusica é a chave
+primária da tabela.
+• Inserir dados na tabela, procurando colocar um gênero de música que tenha mais
+de uma música, e um artista, que tenha mais de uma música cadastrada. Procure
+inserir pelo menos umas 3 músicas.
+• Criar a tabela chamada Album para conter os dados: idAlbum, nome, tipo (digital
+ou físico) e dtLancamento (DATE).
+• Inserir pelo menos 2 albuns;
+• Fazer a modelagem lógica conforme a regra de negócio:
+• 1 album pode ter 1 ou muitas músicas;
+• 1 música é de 1 e somente 1 album;
+Execute os comandos para:
+a) Exibir todos os dados das tabelas separadamente;
+b) Criar a chave estrangeira na tabela de acordo com a regra de negócio;
+c) Atualizar os álbuns de cada música;
+d) Exibir as músicas e seus respectivos álbuns;
+e) Exibir somente o título da música e o nome do seu respectivo álbum;
+f) Exibir os dados das músicas e seu respectivo álbum, de um determinado tipo.
+*/
+
+use sprint1;
+
+CREATE Table musica (
+    idMusica INT PRIMARY KEY AUTO_INCREMENT,
+    titulo VARCHAR(40),
+    artista VARCHAR(40),
+    genero VARCHAR(40)      
+);
+
+INSERT INTO musica (titulo, artista, genero) 
+VALUES 
+('Shape of You', 'Ed Sheeran', 'Pop'),
+('Castle on the Hill', 'Ed Sheeran', 'Pop'),
+('Blinding Lights', 'The Weeknd', 'Pop');
+
+
+CREATE Table album (
+    idAlbum INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(45),
+    tipo VARCHAR (45),
+    dtLancamento DATE,
+    fkMusica INT,
+    constraint chkTipo check(tipo IN('digital','fisico'))
+);
+
+INSERT INTO album (nome, tipo, dtLancamento, fkMusica) 
+VALUES 
+('Divide', 'fisico', '2017-03-03',1),
+('After Hours', 'digital', '2020-03-20',2),
+('Akira', 'digital', '2022-03-20',3);
+
+DROP Table album;
+
+TRUNCATE TABLE album;
+
+drop Table musica;
+
+alter table album add FOREIGN KEY (fkMusica)
+    REFERENCES musica(idMusica);
+
+DESC album;
+
+DESC musica;
+
+SELECT * from album as a 
+    join musica as m
+        on a.fkMusica = m.idMusica;
+
+SELECT * from album as a 
+    join musica as m
+        on a.fkMusica = m.idMusica;
+
+
+
 
