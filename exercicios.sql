8b26908664f3551070de80ffbdcf2197d54c8602
yskillo <matheus.doliveira@sptech.school>
2025-03-23 11:21:24 -0300

    exercicio 03 banco de dados

--- exercicios.sql
+++ exercicios.sql
@@ -1,4 +1,3 @@
-
 create database sprint2;
 
 USE sprint2;
@@ -25,7 +24,7 @@ CREATE TABLE tbAtleta(
 );
 
 ALTER TABLE tbAtleta ADD foreign key (fkPais)
-		references tbPais(idPais);
+        references tbPais(idPais);
 
 INSERT INTO tbAtleta (nomeAtleta, modalidadeAtleta, qntMedalha, fkPais) VALUES
     ('Armando','Futebol', 5, 1),
@@ -45,7 +44,7 @@ on a.fkPais = p.idPais;
  
 select a.nomeAtleta as Atleta, p.nomePais as Pais from tbAtleta as a
 join tbPais as p
-on a.fkPais = p.idPais; 
+on a.fkPais = p.idPais;
 
 select a.nomeAtleta as Atleta, p.nomePais as Pais, p.capitalPais as Capital from tbAtleta as a
 join tbPais as p
@@ -75,7 +74,7 @@ e) Exibir somente o título da música e o nome do seu respectivo álbum;
 f) Exibir os dados das músicas e seu respectivo álbum, de um determinado tipo.
 */
 
-use sprint1;
+use sprint2;
 
 CREATE Table musica (
     idMusica INT PRIMARY KEY AUTO_INCREMENT,
@@ -84,8 +83,8 @@ CREATE Table musica (
     genero VARCHAR(40)      
 );
 
-INSERT INTO musica (titulo, artista, genero) 
-VALUES 
+INSERT INTO musica (titulo, artista, genero)
+VALUES
 ('Shape of You', 'Ed Sheeran', 'Pop'),
 ('Castle on the Hill', 'Ed Sheeran', 'Pop'),
 ('Blinding Lights', 'The Weeknd', 'Pop');
@@ -100,8 +99,8 @@ CREATE Table album (
     constraint chkTipo check(tipo IN('digital','fisico'))
 );
 
-INSERT INTO album (nome, tipo, dtLancamento, fkMusica) 
-VALUES 
+INSERT INTO album (nome, tipo, dtLancamento, fkMusica)
+VALUES
 ('Divide', 'fisico', '2017-03-03',1),
 ('After Hours', 'digital', '2020-03-20',2),
 ('Akira', 'digital', '2022-03-20',3);
@@ -119,14 +118,149 @@ DESC album;
 
 DESC musica;
 
-SELECT * from album as a 
+SELECT * from album as a
     join musica as m
         on a.fkMusica = m.idMusica;
 
-SELECT * from album as a 
+SELECT * from album as a
     join musica as m
         on a.fkMusica = m.idMusica;
 
 
 
+
+-- EXERCICIO 03
+
+USE sprint2;
+show tables;
+
+CREATE TABLE tbPessoa(
+	idPessoa INT PRIMARY KEY AUTO_INCREMENT,
+    nomePessoa VARCHAR(45),
+    cpfPessoa CHAR(11)
+);
+
+CREATE TABLE tbReserva(
+	idReserva INT PRIMARY KEY AUTO_INCREMENT,
+    dtReserva DATETIME,
+    dtRetirada DATETIME,
+    dtDevolucao DATETIME,
+    fk_Pessoa INT,
+		FOREIGN KEY (fk_Pessoa)
+			REFERENCES tbPessoa(idPessoa)
+);
+
+INSERT INTO tbPessoa (nomePessoa, cpfPessoa)VALUES 
+	('Marcia','11122233344'),
+	('Pedro','22233344455'),
+	('Mariana','33344455566'),
+	('Gustavo','44455566677'),
+    ('Paula','55566677788');
+    
+    
+INSERT INTO tbReserva (dtReserva, dtRetirada, dtDevolucao, fk_Pessoa)VALUES 
+	('2000-01-04 15:00:00','2000-01-22 10:00:00', '2000-01-06 12:10:03', 1),
+    ('2000-01-10 13:00:00','2000-01-13 14:00:00', null, 2),
+    ('2000-01-10 13:00:36','2000-01-13 16:20:17', '2000-01-12 14:30:00', 3),
+	('2000-01-22 10:00:13','2000-01-25 12:20:42', null, 4),
+	('2000-01-23 07:00:00','2000-01-25 11:35:00', '2000-01-24 12:30:13', 5);
+    
+
+SELECT * FROM tbReserva;
+
+SELECT p.nomePessoa AS 'Nome Pessoa', r.dtReserva AS 'Data Reserva', r.dtRetirada AS 'Retirada' 
+FROM tbPessoa as p
+	JOIN tbReserva as r
+		ON idPessoa = r.fk_Pessoa;
+        
+        
+SELECT	p.nomePessoa AS 'Nome Pessoa', 
+		r.dtReserva AS 'Data Reserva',  
+        r.dtRetirada AS 'Data Retirada', 
+        ifnull(r.dtDevolucao, 'não houve devolução') AS 'Data Devolução'
+	FROM tbPessoa AS p
+     JOIN tbReserva AS r
+		ON p.idPessoa = r.fk_Pessoa;
+        
+
+SELECT 
+	p.nomePessoa,
+    r.dtReserva,
+    r.dtRetirada,
+    r.dtDevolucao,
+		CASE
+			WHEN r.dtDevolucao IS NULL THEN 'sem devolucao aqui'
+			ELSE 'a devolução foi feita'
+        END AS 'DEVOLUÇÕES'
+    FROM tbPessoa as p
+		JOIN tbReserva as r
+			ON p.idPessoa = r.fk_Pessoa;
+            
+            
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- ultimo exercicio
+
+-- Exercício 6:
+
+-- Regra de negócio:
+
+-- 1 Farmácia tem no mínimo 1 e no máximo 1 endereço, 1 endereço é de no mínimo 1 farmácia e no máximo 1.
+
+-- 1 Farmácia tem no mínimo 1 farmacêutico e no máximo n, 1 farmacêutico é de no mínimo 1 farmácia e no máximo 1.
+
+-- - Criar a modelagem lógica na regra acima, com os campos de sua escolha.
+
+-- - Criar as tabelas;
+
+-- - Inserir 5 registros;
+
+-- - Configurar chave estrangeira;
+
+-- - Exibir dados;
+
+-- - Exibir dados com AS;
+
+-- - Exibir dados com CASE;
+
+-- - Exibir dados com IFNULL;
+
+use sprint2;
+
+CREATE TABLE tbFarmaceutico(
+    idFarmaceutico int PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(45)
+   
+);
+
+CREATE TABLE tbEndereco(
+    idEndereço int PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(45),
+    cep char(9),
+    numero VARCHAR(5)
+);
+
+CREATE TABLE tbFarmacia(
+    idFarmacia int PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(45),
+    cnpj char(18),
+    CONSTRAINT fkEndereço
+        Foreign Key (tbEnderco)
+            REFERENCES tbEndereco(idEndereco)
+);
 
